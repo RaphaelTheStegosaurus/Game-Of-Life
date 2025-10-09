cca873f9149759a13f855ce16290f6ee30205248
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2025-10-09 15:36:39 -0700

    Working in system of check neighbor cells Adding function Toroidal, FindIndex and FindNeighbor

--- app/components/cell-manager.tsx
+++ app/components/cell-manager.tsx
@@ -46,12 +46,51 @@ function Cell_Grid_Manager({ cols, rows }: Props) {
     3
     4
     x=(3,2) = 5+5+(1+1+1+1+0)-1 = 13
+    
+    00 01 02 03 04 05 
+    10 11 12 13 14 15
+    20 21 22 23 24 25
+    
+    [r-1,c-1]   [r-1,c+0]   [r-1,c+1] 
+    [r+0,c-1]   [r+0,c+0]   [r+0,c+1] 
+    [r+1,c-1]   [r+1,c+0]   [r+1,c+1] 
+
     */
-   
+  };
+  const findIndexIntoTheGrid = (col: number, row: number) => {
+    const COUNT_OF_COLUMNS = col - (col == 0 ? 0 : 1);
+    const newIndex: number = cols * row + COUNT_OF_COLUMNS;
+    return newIndex;
+  };
+  const checkNeighborsCells = (col: number, row: number) => {
+    const MAP_NEIGHBOR = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, -1],
+      //   [0, 0], es el mismo
+      [0, 1],
+      [1, -1],
+      [1, 0],
+      [1, 1],
+    ];
+    const listOfNeghbors = MAP_NEIGHBOR.map((value, index) => {
+      const colPointer = ToroidalGrid(col + value[0], cols);
+      const rowPoiter = ToroidalGrid(row + value[1], rows);
+      return [colPointer, rowPoiter];
+    });
+  };
+  const ToroidalGrid = (value: number, SizeAxis: number) => {
+    if (value < 0) {
+      return SizeAxis - 1;
+    } else if (value >= SizeAxis) {
+      return 0;
+    } else {
+      return value;
+    }
   };
   useEffect(() => {
     console.log(GridCellState);
-    
   }, [GridCellState]);
 
   return (
