efaf4160537e077a41d8dced975cb7012f612e26
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2025-10-13 12:58:05 -0700

    Working in reset Button, Cols Button and Rows Button

--- app/components/controls.tsx
+++ app/components/controls.tsx
@@ -3,20 +3,33 @@ interface Props {
   setIsRunning: (isRunning: boolean) => void;
   onNextStep: () => void;
 }
-
+import Styles from "./controlls.module.css";
 export default function Controls({
   isRunning,
   setIsRunning,
   onNextStep,
 }: Props) {
   return (
-    <div>
-      <button onClick={() => setIsRunning(!isRunning)}>
-        {isRunning ? "⏸️ Detener" : "▶️ Iniciar"}
+    <aside className={Styles.Panel}>
+      <button
+        className={Styles.ControlButton}
+        onClick={() => setIsRunning(!isRunning)}
+      >
+        {isRunning ? "⏸️ Stop" : "▶️ Start"}
       </button>
-      <button onClick={onNextStep} disabled={isRunning}>
-        ➡️ Siguiente Paso
+      <button
+        className={Styles.ControlButton}
+        onClick={onNextStep}
+        disabled={isRunning}
+      >
+        ➡️ Next Step
       </button>
-    </div>
+      <button className={Styles.ControlButton}>
+        {" "}
+        <span className={Styles.Icon}>↺</span> Reset
+      </button>
+      <input type="numeric" step={1} max={20} min={5} value={12} />
+      <input type="numeric" step={1} max={20} min={5} value={6} />
+    </aside>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,6 @@ export default function Home() {
         setIsRunning={setIsRunning}
         onNextStep={() => gridManagerRef.current?.checkGrid()}
       />
-      <section className={`${styles.panel}`}></section>
     </>
   );
 }
